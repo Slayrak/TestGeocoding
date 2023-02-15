4f7c111d287237a8e5cc5ae3f35714a038623b5f
Максим Шимків <322topgamerintheworld228@gmail.com>
2023-02-16 00:16:19 +0200

    Add project

---
+++ TestGeocoding/TestGeocoding/DataAccess/CacheContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestGeocoding.DataAccess
+{
+    public class CacheContext : DbContext
+    {
+        public CacheContext(DbContextOptions<CacheContext> opts)
+            :base(opts) { }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+
+    }
+}
