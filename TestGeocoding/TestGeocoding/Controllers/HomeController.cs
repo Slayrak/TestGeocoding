f834dab8f93505380dfec29fe1482e58b201b1ac
Максим Шимків <322topgamerintheworld228@gmail.com>
2023-02-17 13:47:39 +0200

    Add geocoding

--- TestGeocoding/TestGeocoding/Controllers/HomeController.cs
+++ TestGeocoding/TestGeocoding/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.IO;
 using TestGeocoding.Models;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TestGeocoding.Controllers
 {
@@ -27,6 +31,25 @@ namespace TestGeocoding.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpGet]
+        public IActionResult GetLongLat(string street)
+        {
+            var result = new System.Net.WebClient().DownloadString(
+               $"https://maps.googleapis.com/maps/api/geocode/json?address={street},&key=AIzaSyDUZFWbzmf7Nyf4F3kbX-IdOPhDNYj0l1A");
+
+            return Json(result);
+        }
+
+        [HttpGet]
+        public IActionResult GetAddress(string latitude, string longitude) 
+        {
+            var result = new System.Net.WebClient().DownloadString(
+               $"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key=AIzaSyDUZFWbzmf7Nyf4F3kbX-IdOPhDNYj0l1A&location_type=ROOFTOP");
+
+            return Json(result);
         }
     }
 }
